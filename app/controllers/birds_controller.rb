c5ecb1121d1e4a747fac14d988b8bf6fa2713995
Elijah Samuels <elijahsamuels@gmail.com>
2021-02-24 16:32:32 -0700

    commit tree 3831d6b508fb2d10f097d907db6c2c2bd1aecbe2

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,27 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    # render json: birds
+    render json: birds, only: [:id, :name, :species]
   end
+
+  # def show
+  #   bird = Bird.find_by(id: params[:id])
+  #   render json: bird
+  # end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    
+    if bird
+      render json: { id: bird.id, name: bird.name, species: bird.species }
+
+    else # the error handling if NIL birds
+      render json: { message: 'Bird not found' }
+    end
+     # render json: bird.slice(:id, :name, :species)
+
+  end
+
+
 end
